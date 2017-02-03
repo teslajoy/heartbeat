0159e8a0dc2756e49da53fe5f7ded5937804f96c
Nasim Sanati <nasim@plenary.org>
2017-02-02 18:02:45 -0800

    initial code

--- Polar-HeartRate-Receiver/Polar-HeartRate-Code/_28048-Polar-HeartRate-Arduino/_28048-Polar-HeartRate-Arduino.ino
+++ Polar-HeartRate-Receiver/Polar-HeartRate-Code/_28048-Polar-HeartRate-Arduino/_28048-Polar-HeartRate-Arduino.ino
@@ -1,21 +1,21 @@
-//Definitions  
+//Definitions and port-number constants 
 const int HR_RX = 7;
 const int ledPin = 13;
 byte oldSample, sample;
 
 void setup() {
+   
   Serial.begin(9600);
   pinMode (HR_RX, INPUT);  //Signal pin to input  
-  delay(3000);
+  delay(3000);             //Wait to print test 
   Serial.println("Waiting for heart beat...");
 
   //Wait until a heart beat is detected  
   while (!digitalRead(HR_RX)) {};
   
   pinMode(ledPin, OUTPUT);
-  delay(3000);
+  delay(3000);           //Wait to print test 
   Serial.println ("Heart beat detected!");
-  
 }
 
 void loop() {
@@ -23,11 +23,10 @@ void loop() {
   sample = digitalRead(HR_RX);  //Store signal output 
   if (sample && (oldSample != sample)) {
     digitalWrite(ledPin, HIGH);   // set the LED on
-    delay(10);                    // need to wait between on and off (can we faid here?)
+    delay(10);                    // need to wait between on and off (can we fade here?)
     digitalWrite(ledPin, LOW);    // set the LED off
     Serial.println("Beat");
   }
   oldSample = sample;           //Store last signal received 
-  
 }
   
